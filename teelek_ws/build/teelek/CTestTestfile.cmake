e167368bb1dc970def13ca0491916d950af9d1e0
Kittifirst <newfirst47@gmail.com>
2025-09-08 17:21:36 +0000

    new update

---
+++ teelek_ws/build/teelek/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kittibot/teelek_rb/teelek_ws/src/teelek
+# Build directory: /home/kittibot/teelek_rb/teelek_ws/build/teelek
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
